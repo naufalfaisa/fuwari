01ad0c74de3f78c6314a14c31c0308288f82df55
Naufal <naufalfaisa386@gmail.com>
2025-08-30 01:18:13 +0700

    first commit

---
+++ src/config.ts
@@ -0,0 +1,93 @@
+import type {
+	ExpressiveCodeConfig,
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
+
+export const siteConfig: SiteConfig = {
+	title: "ウエルカム",
+	subtitle: "Blog website",
+	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	themeColor: {
+		hue: 0, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: false, // Hide the theme color picker for visitors
+	},
+	banner: {
+		enable: true,
+		src: "assets/images/seaside_by_mma1r_dk26pw5.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		credit: {
+			enable: true, // Display the credit text of the banner image
+			text: "seaside", // Credit text to be displayed
+			url: "https://www.pixiv.net/artworks/132178898", // (Optional) URL link to the original artwork or artist's page
+		},
+	},
+	toc: {
+		enable: true, // Display the table of contents on the right side of the post
+		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+	},
+	favicon: [
+		// Leave this array empty to use the default favicon
+		// {
+		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		// }
+	],
+};
+
+export const navBarConfig: NavBarConfig = {
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		{
+			name: "GitHub",
+			url: "https://github.com/naufalfaisa", // Internal links should not include the base path, as it is automatically added
+			external: true, // Show an external link icon and will open in a new tab
+		},
+	],
+};
+
+export const profileConfig: ProfileConfig = {
+	avatar: "assets/images/20250823_043236.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Naufal Faisa",
+	bio: "Lonely",
+	links: [
+		{
+			name: "X",
+			icon: "fa6-brands:x-twitter", // icon X (Twitter baru)
+			url: "https://x.com/naufalfaisa",
+		},
+		{
+			name: "Instagram",
+			icon: "fa6-brands:instagram",
+			url: "https://instagram.com/naufalfaisa_",
+		},
+		{
+			name: "Facebook",
+			icon: "fa6-brands:facebook",
+			url: "https://facebook.com/naufalfaisa17",
+		},
+		{
+			name: "LinkedIn",
+			icon: "fa6-brands:linkedin",
+			url: "https://www.linkedin.com/in/naufal-faisa-071239312",
+		},
+	],
+};
+
+export const licenseConfig: LicenseConfig = {
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+
+export const expressiveCodeConfig: ExpressiveCodeConfig = {
+	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
+	// Please select a dark theme, as this blog theme currently only supports dark background color
+	theme: "github-dark",
+};
---
+++ src/content/spec/about.md
@@ -0,0 +1,12 @@
+# About
+This is the personl site for [Naufal](https://github.com/naufalfaisa).
+
+> ![Last.fm playing](https://lastfm-recently-played.vercel.app/api?user=naufalfaisa&width=500)
+
+::github{repo="naufalfaisa/naufalfaisa"}
+
+> ### Sources of images used in this site
+> - [seaside](https://www.pixiv.net/artworks/132178898) by [mmAir](https://www.pixiv.net/users/39363802)
+<!-- > - [Unsplash](https://unsplash.com/)
+> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
+> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty) -->
